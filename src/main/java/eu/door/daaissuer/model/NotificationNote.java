4099ec79dbb1c532182cd2f16d85f34617169df2
menelaosgiannopoulos <mgiannopoulos@ubitech.eu>
2022-06-02 18:22:33 +0300

    Changed NotificationNote

--- src/main/java/eu/door/daaissuer/model/NotificationNote.java
+++ src/main/java/eu/door/daaissuer/model/NotificationNote.java
@@ -1,38 +1,19 @@
 package eu.door.daaissuer.model;
 
+import com.google.gson.Gson;
+
 public class NotificationNote {
-    private byte[] signedTpmNonce;
-    private byte[] tpmNonce;
-    private String token;
+    private String signedTpmNonce;
 
     public NotificationNote(DaaRegister daaRegister) {
-        this.signedTpmNonce = daaRegister.getSignedTpmNonce();
-        this.tpmNonce = daaRegister.getTpmNonce();
-        this.token = daaRegister.getRegnObject()
-                .getToken();
+        this.signedTpmNonce = new Gson().toJson(daaRegister.getSignedTpmNonce());
     }
 
-    public byte[] getSignedTpmNonce() {
+    public String getSignedTpmNonce() {
         return signedTpmNonce;
     }
 
-    public void setSignedTpmNonce(byte[] signedTpmNonce) {
+    public void setSignedTpmNonce(String signedTpmNonce) {
         this.signedTpmNonce = signedTpmNonce;
-    }
-
-    public byte[] getTpmNonce() {
-        return tpmNonce;
-    }
-
-    public void setTpmNonce(byte[] tpmNonce) {
-        this.tpmNonce = tpmNonce;
-    }
-
-    public String getToken() {
-        return token;
-    }
-
-    public void setToken(String token) {
-        this.token = token;
     }
 }
